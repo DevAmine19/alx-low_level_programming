b51d11782dc79794a770143cd22a860dff6e3ccb
amine <aminee4579@gmail.com>
2023-09-12 10:00:00 -0700

    adda

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * int _abs- function print an intger zero positive or negative 
+ * _abs- function print an intger zero positive or negative
  * @n: is an integer
  * Return: Always 0 (Success)
  */
 int _abs(int n)
 {
-	if (n > 0 || n== 0)
+	if (n > 0 || n == 0)
 		return (n);
-	else 
+	else
 		return ((-1) * n);
 }
 
