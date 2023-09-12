823e2a655a5c40d60b9b9f5bb4f05569550008e6
amine <aminee4579@gmail.com>
2023-09-12 08:22:07 -0700

    adda

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * islower- function print the alphabet is lowercase or not 
- *
+ * islower - function print the alphabet is lowercase or not
+ * c is an integer related of an charcter
  * Return: Always 0 (Success)
  */
 int _islower(int c)
 {
 	if (c < 123 && c > 96)
 		return (1);
-	else 
+	else
 		return (0);
 }
