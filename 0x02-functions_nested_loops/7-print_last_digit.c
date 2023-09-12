d6e80a02d4372294af32042e5215e8fb1b78d91c
amine <aminee4579@gmail.com>
2023-09-12 10:38:52 -0700

    adda

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,12 @@
  */
 int print_last_digit(int n)
 {
-         int s;
-	 
-         if (n < 0)
-        	s = (-1) * (n % 10);
-         else
-        	s = n % 10;
-        _putchar(s + '0');
+	int s;
+
+	if (n < 0)
+		s = (-1) * (n % 10);
+	else
+		s = n % 10;
+	_putchar(s + '0');
 return (s);
 }
