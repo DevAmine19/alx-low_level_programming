a83542dc6c34109cb4bbe0c6e9af3b4c0577b804
amine <aminee4579@gmail.com>
2023-09-12 08:46:59 -0700

    adda

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * islower- function print the alphabet is lowercase or uppercase
+ * _isalpha - function print the alphabet is lowercase or uppercase
  * @c: is an integer
  * Return: Always 0 (Success)
  */
 int _isalpha(int c)
 {
-	if ((c > 64 && c < 91) || (c > 96 && c < 123))
+	if (c > 64 && c < 91 || c > 96 && c < 123)
 	       return (1);
 	else
 		return (0);
