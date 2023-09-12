0fb2609180230954069f5a531cc8ad4e4d469502
amine <aminee4579@gmail.com>
2023-09-12 09:41:51 -0700

    adda

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,15 +1,24 @@
 #include "main.h"
 /**
- * print_sign- function print an intger zero positive or negative 
+ * print_sign- function print an intger zero positive or negative
  * @n: is an integer
  * Return: Always 0 (Success)
  */
 int print_sign(int n)
 {
 	if (n > 0)
+	{
+		_putchar(43);
 		return (1);
+	}
 	else if (n == 0)
+	{
+		_putchar(48);
 		return (0);
-	else 
+	}
+	else
+	{
+		_putchar(45);
 		return (-1);
+	}
 }
