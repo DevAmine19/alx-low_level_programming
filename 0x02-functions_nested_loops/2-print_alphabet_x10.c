364b418b93144cabff901092b2276cffc4ae9871
amine <aminee4579@gmail.com>
2023-09-12 07:42:48 -0700

    adda

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - function print the alphabet in lowercase 10 
+ * print_alphabet_x10-function print the alphabet in lowercase  
  *
  * Return: Always 0 (Success)
  */
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	char str[] = "abcdefghijklmnopqrstuvwxyz";
 	int i, j;
 
-	for (i = 0; i< 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 26; j++)
 		{
