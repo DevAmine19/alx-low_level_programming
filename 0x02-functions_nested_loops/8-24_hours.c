1be397931986368d92d220efea4880226b0c128b
amine <aminee4579@gmail.com>
2023-09-12 15:50:32 -0700

    adda

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * jack_bauer - function print time
- * 
+ *
  * Return: Always 0 (Success)
  */
 void jack_bauer(void)
 {
 	int hr, min;
+
 	for (hr = 0; hr <= 23; hr++)
 	{
 		for (min = 0; min <= 59; min++)
