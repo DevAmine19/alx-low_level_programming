36290c836726547880bdf0fdeecdfccacb64a121
amine <aminee4579@gmail.com>
2023-09-12 17:35:50 -0700

    adda

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,11 +15,11 @@ void print_to_98(int n)
 			printf("%d, ", i);
 		}
 	}
-	else 
+	else
 	{
 		for (i = n; i > 98; i--)
 		{
-	                printf("%d, ", i);
+			printf("\d, ", i);
 		}
 	}
 	printf("98\n");
