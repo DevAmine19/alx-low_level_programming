6493926a7835c60581bfc52dc8ac2227b6fb79a4
amine <aminee4579@gmail.com>
2023-09-12 17:19:58 -0700

    adda

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_to_98- function print addition from a number to 98
+ * @n:an intger
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d, ", i);
+		}
+		printf("\n");
+	}
+	else 
+	{
+		for (i = 0; i > 99; i--)
+		{
+	                printf("%d, ", i);
+		}
+		printf("\n");
+	}
+}
