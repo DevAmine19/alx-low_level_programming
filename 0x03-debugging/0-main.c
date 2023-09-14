1a71c4c5bb0a35982e1efcebd5e4ed610f8bd96f
amine <aminee4579@gmail.com>
2023-09-14 15:43:14 -0700

    adda

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
-* positive_or_negative - function that print nmber negative or positive
-* @i: an intger
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
-void positive_or_negative(int i)
+int main(void)
 {
-	if (i > 0)
-		printf("%d is positive\n");
-	else if (i == 0)
-		printf("0 is zero\n");
-	else
-		printf("%d is negative\n");
+	int i;
+
+	i = 98 % 2;
+	positive_or_negative(i);
+return (0);
 }
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* positive_or_negative - function that print nmber negative or positive
+* @i: an intger
+*/
+void positive_or_negative(int i)
+{
+        if (i > 0)
+                printf("%d is positive\n", i);
+        else if (i == 0)
+                printf("0 is zero\n");
+        else
+                printf("%d is negative\n", i);
+}
